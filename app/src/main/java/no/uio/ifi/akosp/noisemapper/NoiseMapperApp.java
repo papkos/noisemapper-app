9c96eb13daef2deb4eb8a879b0c25c86409106b0
Ákos Pap <papkos@gmail.com>
2016-04-17 14:31:49 +0200

    init Project setup, empty activity, Application file.

---
+++ app/src/main/java/no/uio/ifi/akosp/noisemapper/NoiseMapperApp.java
@@ -0,0 +1,11 @@
+package no.uio.ifi.akosp.noisemapper;
+
+import android.app.Application;
+
+/**
+ * Created on 2016.04.17..
+ *
+ * @author Ákos Pap
+ */
+public class NoiseMapperApp extends Application {
+}
---
+++ app/src/main/java/no/uio/ifi/akosp/noisemapper/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package no.uio.ifi.akosp.noisemapper.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import no.uio.ifi.akosp.noisemapper.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
